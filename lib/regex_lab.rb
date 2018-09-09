99337ac265ee1bb7d5ab671389b9564d5c2a37e6
Ashleigh Chun <ashleigh.chun@wework.com>
2018-09-09 05:58:13 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,7 @@ def words_five_letters_long(text)
 end
     
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if word.match(\b[A-Z]\b) 
+  if word.match([A-Z]) 
     return true
   else 
     return false
