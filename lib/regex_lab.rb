461967eeb832c292fc0ff775b9516e2fa4a0fb7b
Ashleigh Chun <ashleigh.chun@wework.com>
2018-09-09 04:41:34 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,15 +1,21 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/\b[aeiou]\w*/i)
+    return true
+  else 
+    return false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  if text.scan()
+end
 end
 
 def words_five_letters_long(text)
-
+  if text.scan(/^\w{5}\s/)
+    return text.scan()
+  end
 end
-
 def first_word_capitalized_and_ends_with_punctuation?(text)
 
 end
@@ -17,3 +23,5 @@ end
 def valid_phone_number?(phone)
 
 end
+
+
