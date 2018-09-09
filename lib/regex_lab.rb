d486e6860820a4e7a93cc590135e0a76f0f6c466
Ashleigh Chun <ashleigh.chun@wework.com>
2018-09-09 06:28:07 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,5 @@
 def starts_with_a_vowel?(word)
-  if word.match(/\b[aeiou]\w*/i)
-    return true
-  else 
-    return false
-  end
+  !!word.match(/\b[aeiou]\w*/i)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
@@ -15,11 +11,7 @@ def words_five_letters_long(text)
 end
     
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/^[A-Z]\D+[!.?]$/) 
-    return true
-  else 
-    return false
-  end
+  !!text.match(/^[A-Z]\D+[!.?]$/) 
 end
 
 def valid_phone_number?(phone)
